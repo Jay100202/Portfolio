42369070e55f67a060bdc6cea92e8aefaa8013e5
Jay Mandaliya <bhailumandaliya11@gmail.com>
2024-07-27 00:35:04 +0530

    first commit

---
+++ frontend_react/src/components/SocialMedia.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import { BsLinkedin, BsEnvelope } from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa";
+
+const SocialMedia = () => {
+  return (
+    <div className="app__social">
+      <div>
+        <a
+          href="www.linkedin.com/in/jay-mandaliya-054317210"
+          target="_blank"
+          rel="noreferrer"
+          className="app__flex"
+        >
+          <BsLinkedin />
+        </a>
+      </div>
+     
+      <div>
+  <a
+    href="mailto:jaymandaliya1212@gmail.com"
+    className="app__flex"
+  >
+    <BsEnvelope />
+  </a>
+</div>
+    </div>
+  );
+};
+
+export default SocialMedia;
